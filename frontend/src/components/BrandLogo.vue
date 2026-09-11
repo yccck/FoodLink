1a85cffcc002af135294030b371d9e2d33123d6a
nicoleathenatian <1786620982@qq.com>
2026-09-11 20:08:09 +0800

    新增logo组件

---
+++ frontend/src/components/BrandLogo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="['brand-logo', `brand-logo--${size}`]">
+    <img class="brand-logo__image" src="/foodlink-logo.png" alt="食愿 FoodLink" />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  size: {
+    type: String,
+    default: 'medium',
+    validator: (value) => ['small', 'medium', 'large'].includes(value)
+  }
+})
+</script>
